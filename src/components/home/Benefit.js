4687be381134bb1bc776dcee9ae0f2b9395f3bc9
WahyuS002 <syahputrawahyu61@gmail.com>
2022-01-09 10:09:00 +0700

    :zap:

--- src/components/home/Benefit.js
+++ src/components/home/Benefit.js
@@ -8,17 +8,15 @@ export default function Benefit() {
                 <div className="md:col-span-2">
                     <img src={BenefitImage} alt="" />
                 </div>
-                <div className="md:col-span-5 col-start-4 mt-4 md:mt-0">
+                <div className="md:col-span-5 md:col-start-4 mt-4 md:mt-0">
                     <h2 className="text-3xl md:text-5xl font-semibold">Benefit for the holders</h2>
                     <p className="mt-4 text-base md:text-lg text-gray-300 text-justify leading-relaxed">
-                        By owning Degen Goat, you'll become a part of <span className="bg-primary px-2 py1 rounded-lg text-semi-black font-semibold">Respected Alpha</span> on Algorand, also you will
-                        get full access to make Degen Goat <span className="bg-primary px-2 py1 rounded-lg text-semi-black font-semibold">derivative art</span>. There will be a Degen Goat Derivative
-                        and Collab edition, where all derivative and collab between us become one. 90% of total sales will be transfered to the creator, 10% will go to the Degen Goat Bank. Degen Goat
-                        Bank will be use wisely according to the wishes of all collectors.
+                        By owning Degen Goat, you'll become a part of Respected Alpha on Algorand, also you will get full access to make Degen Goat derivative art. There will be a Degen Goat
+                        Derivative and Collab edition, where all derivative and collab between us become one. 90% of total sales will be transfered to the creator, 10% will go to the Degen Goat Bank.
+                        Degen Goat Bank will be use wisely according to the wishes of all collectors.
                         <br />
                         <br />
-                        <span className="bg-primary px-2 py1 rounded-lg text-semi-black font-semibold">We also willing to help you</span> as a holder to start your own nft project. If you already have
-                        a raw materials, we are happy to help to generate your art and batch minting.
+                        We also willing to help you as a holder to start your own nft project. If you already have a raw materials, we are happy to help to generate your art and batch minting.
                     </p>
                 </div>
             </div>
